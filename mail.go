fafa25d3ca3b6da719e56e381d3304d26f906a32
Sean Zhang <sean7218@gmail.com>
2017-10-06 23:52:12 -0400

    added mail for reset password

---
+++ mail.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"net/smtp"
+	"fmt"
+)
+
+func setupMailServer() {
+
+	// Set up authentication information.
+	auth := smtp.PlainAuth("", "sean7218@gmail.com", "******", "smtp.gmail.com")
+
+
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one step.
+	to := []string{"recipient@example.net"}
+	msg := []byte("To: recipient@example.net\r\n" +
+		"Subject: discount Gophers!\r\n" +
+		"\r\n" +
+		"This is the email body.\r\n")
+	err := smtp.SendMail("mail.example.com:25", auth, "sender@example.org", to, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	// Connect to the remote SMTP server.
+	c, err := smtp.Dial("smtp.gmail.com:25")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set the sender and recipient first
+	if err := c.Mail("sean7218@gmail.com"); err != nil {
+		log.Fatal(err)
+	}
+	if err := c.Rcpt("recipient@example.net"); err != nil {
+		log.Fatal(err)
+	}
+
+	// Send the email body.
+	wc, err := c.Data()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = fmt.Fprintf(wc, "This is the email body")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = wc.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Send the QUIT command and close the connection.
+	err = c.Quit()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
